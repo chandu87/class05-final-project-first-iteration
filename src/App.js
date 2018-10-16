b1bdbac5becd48cb576d8a2062917394d695c8b4
chandu <chandrasr.palle@gmail.com>
2018-10-16 09:38:21 +0200

    code refactored remove unneccessary files

--- src/App.js
+++ src/App.js
@@ -4,20 +4,22 @@ import Calendar from "./components/calendar/Calendar";
 import SiteHeader from "./components/site-header/SiteHeader";
 import SideBar from "./components/sidebar/sidebar";
 import './App.css';
-const myText=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur illo quo tempora explicabo omnis mollitia ipsam, pariatur totam nam! Quibusdam error unde mollitia optio quo porro atque, obcaecati ad?Necessitatibus earum inventore mollitia doloremque, perferendisquidem, illum distinctio est quos rem fugit expedita unde amet nihil velit perspiciatis. Non, nihil?";
 import FooterPage from './components/Footer/FooterPage';
 import Dropdown from './components/DropDown/Dropdown';
+
+const myText=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur illo quo tempora explicabo omnis mollitia ipsam, pariatur totam nam! Quibusdam error unde mollitia optio quo porro atque, obcaecati ad?Necessitatibus earum inventore mollitia doloremque, perferendisquidem, illum distinctio est quos rem fugit expedita unde amet nihil velit perspiciatis. Non, nihil?";
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <SiteHeader />
-      <Header />
-        <Calendar />
-        <SideBar text={myText}/>
-        <Dropdown />
-          <FooterPage />
+        <SiteHeader />
+        <Header />
+          <Calendar />
+          <SideBar text={myText}/>
+          <Dropdown />
+            <FooterPage />
       </div>
     );
   }
--- src/components/SelectBox.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class SelectBox extends React.Component{
-    render(){
-        return "SelectBox";
-    }
-}
-
-export default SelectBox;
